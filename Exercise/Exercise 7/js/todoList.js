780542bc76017e98be3daebbadfb2e7796c6778e
monocosmo <shuchiwu@gmail.com>
2018-06-29 21:37:26 -0400

    Add files via upload

--- Exercise/Exercise 7/js/todoList.js
+++ Exercise/Exercise 7/js/todoList.js
@@ -6,15 +6,23 @@ var input = prompt("What would you like to do?");
 while(input !== "quit") {
   if(input === "list") {
     // "list"
+    console.log("**********");
     for(i = 0; i < todos.length; i++) {
-      console.log(todos[i]);
+      console.log(i + ": " + todos[i]);
     }
+    console.log("**********");
   } else if(input === "new") {
     // "new"
     var newItem = prompt("What would you like to add in the list?");
     todos.push(newItem);
+  } else if(input === "delete") {
+    // "delete"
+    var index = prompt("Enter index of todo to delete:");
+    todos.splice(index, 1);
+  } else {
+    alert("Invalid Input!!")
   }
   var input = prompt("What would you like to do?");
 }
-
-alert("Quit the Todo List")
+// "quit"
+alert("Quit the Todo List.");
