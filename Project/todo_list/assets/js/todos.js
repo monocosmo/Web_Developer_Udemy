04db44551acadff9ce9ea5b9be95e569260fff22
monocosmo <shuchiwu@gmail.com>
2018-07-10 22:09:55 -0400

    Add files via upload

--- Project/todo_list/assets/js/todos.js
+++ Project/todo_list/assets/js/todos.js
@@ -1,12 +1,25 @@
 // Check off todos by clicking
-$("li").click(function(){
+$("ul").on("click", "li", function(){
   $(this).toggleClass("completed");
 });
 
 // Delete todos
-$("span").click(function(event){
+$("ul").on("click", "span", function(event){
   $(this).parent().fadeOut(500, function(){
     $(this).remove();
   });
   event.stopPropagation(); // Stop bubbling
 })
+
+// Create new todos
+$("input[type='text']").keypress(function(event){
+  if(event.which === 13){
+    var todoText = $(this).val();
+    $(this).val(""); // Clear the input
+    $("ul").append("<li><span><i class='far fa-trash-alt'></i></span> " + todoText + "</li>");
+  }
+})
+
+$(".fa-edit").click(function(){
+  $("input[type='text']").fadeToggle();
+});
